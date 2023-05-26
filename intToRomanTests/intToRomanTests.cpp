1175dceb92f936806cfc1672dfd445a7d512bce6
ViktorKutuzov <125211032+ViktorKutuzov@users.noreply.github.com>
2023-05-26 21:20:58 +0300

    Скорректированы тесты для функции intToRoman

--- intToRomanTests/intToRomanTests.cpp
+++ intToRomanTests/intToRomanTests.cpp
@@ -10,59 +10,59 @@ namespace intToRomanTests
 	{
 	public:
 		
-		TEST_METHOD(simpleSingleDigit)
+		TEST_METHOD(simpleSingleDigit) //Простое однозначное число
 		{
-			string result = intToRoman(1);
-			string expResult = "I";
+			string result = intToRoman(1),
+				expResult = "I";
 			Assert::AreEqual(expResult, result);
 		}
 
-		TEST_METHOD(simpleTwoDigitNumber)
+		TEST_METHOD(simpleTwoDigitNumber) //Простое двузначное число
 		{
-			string result = intToRoman(50);
-			string expResult = "L";
+			string result = intToRoman(50),
+				expResult = "L";
 			Assert::AreEqual(expResult, result);
 		}
 
-		TEST_METHOD(simpleThreeDigitNumber)
+		TEST_METHOD(simpleThreeDigitNumber) //Простое трёхзначное число
 		{
-			string result = intToRoman(500);
-			string expResult = "D";
+			string result = intToRoman(500),
+				expResult = "D";
 			Assert::AreEqual(expResult, result);
 		}
 
-		TEST_METHOD(simpleFourDigitNumber)
+		TEST_METHOD(simpleFourDigitNumber) //Простое четырехзначное число
 		{
-			string result = intToRoman(1000);
-			string expResult = "M";
+			string result = intToRoman(1000),
+				expResult = "M";
 			Assert::AreEqual(expResult, result);
 		}
 
-		TEST_METHOD(compositeSingleDigitNumber)
+		TEST_METHOD(compositeSingleDigitNumber) //Составное однозначное число
 		{
-			string result = intToRoman(8);
-			string expResult = "VIII";
+			string result = intToRoman(8),
+				expResult = "VIII";
 			Assert::AreEqual(expResult, result);
 		}
 
-		TEST_METHOD(compositeTwoDigitNumber)
+		TEST_METHOD(compositeTwoDigitNumber) //Составное двузначное число
 		{
-			string result = intToRoman(22);
-			string expResult = "XXII";
+			string result = intToRoman(22),
+				expResult = "XXII";
 			Assert::AreEqual(expResult, result);
 		}
 
-		TEST_METHOD(compositeThreeDigitNumber)
+		TEST_METHOD(compositeThreeDigitNumber) //Составное трёхзначное число
 		{
-			string result = intToRoman(999);
-			string expResult = "CMXCIX";
+			string result = intToRoman(999),
+				expResult = "CMXCIX";
 			Assert::AreEqual(expResult, result);
 		}
 
-		TEST_METHOD(compositeFourDigitNumber)
+		TEST_METHOD(compositeFourDigitNumber) //Составное четырехзначное число
 		{
-			string result = intToRoman(3456);
-			string expResult = "MMMCDLVI";
+			string result = intToRoman(3456),
+				expResult = "MMMCDLVI";
 			Assert::AreEqual(expResult, result);
 		}
 	};
