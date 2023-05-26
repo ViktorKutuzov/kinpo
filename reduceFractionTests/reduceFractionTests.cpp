b9a9b481c130da2613c0373879d82e7e48fc6956
ViktorKutuzov <125211032+ViktorKutuzov@users.noreply.github.com>
2023-05-26 22:25:14 +0300

    Реализованы тесты для функции reduceFraction

--- reduceFractionTests/reduceFractionTests.cpp
+++ reduceFractionTests/reduceFractionTests.cpp
@@ -11,8 +11,58 @@ namespace reduceFractionTests
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(fractionReducedWithoutErrorTest1) //ƒробь сокращаетс€ без ошибок
 		{
+			string romanNumerator = "XIV",
+				romanDenominator = "II",
+				expResult = "VII",
+				result = reduceFraction(romanNumerator, romanDenominator);
+			Assert::AreEqual(expResult, result);
+		}
+
+		TEST_METHOD(fractionReducedWithoutErrorTest2) //ƒробь сокращаетс€ без ошибок
+		{
+			string romanNumerator = "MMMCD",
+				romanDenominator = "XVII",
+				expResult = "CC",
+				result = reduceFraction(romanNumerator, romanDenominator);
+			Assert::AreEqual(expResult, result);
+		}
+
+		TEST_METHOD(reducedFractionHasDenominatorI) //—окращенна€ дробь имеет знаменатель УIФ
+		{
+			string romanNumerator = "X",
+				romanDenominator = "X",
+				expResult = "I",
+				result = reduceFraction(romanNumerator, romanDenominator);
+			Assert::AreEqual(expResult, result);
+		}
+
+		TEST_METHOD(fractionHasDenominatorI) //ƒробь имеет знаменатель УIФ
+		{
+			string romanNumerator = "X",
+				romanDenominator = "I",
+				expResult = "X",
+				result = reduceFraction(romanNumerator, romanDenominator);
+			Assert::AreEqual(expResult, result);
+		}
+
+		TEST_METHOD(fractionCantBeReducedTest1) //ƒробь нельз€ сократить
+		{
+			string romanNumerator = "XIII",
+				romanDenominator = "XI",
+				expResult = "XIII/XI",
+				result = reduceFraction(romanNumerator, romanDenominator);
+			Assert::AreEqual(expResult, result);
+		}
+
+		TEST_METHOD(fractionCantBeReducedTest2) //ƒробь нельз€ сократить
+		{
+			string romanNumerator = "MMMCMXCIX",
+				romanDenominator = "XIII",
+				expResult = "MMMCMXCIX/XIII",
+				result = reduceFraction(romanNumerator, romanDenominator);
+			Assert::AreEqual(expResult, result);
 		}
 	};
 }
